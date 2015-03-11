6b6c684be005fbc8d37f9cd75ee42052012de4a6
Rizart Dokollari <r.dokollari@gmail.com>
2015-03-11 19:22:02 +0000

    Fix broken style link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,19 +5,19 @@
     <title>StatsApp | {!! $title !!} </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.2 -->
-    {!! HTML::style('packages/bower/bootstrap/dist/css/bootstrap.min.css', true) !!}
+    {!! HTML::style('packages/bower/bootstrap/dist/css/bootstrap.min.css', [], true) !!}
     <!-- Font Awesome Icons -->
-    {!! HTML::style('packages/bower/fontawesome/css/font-awesome.min.css', true) !!}
+    {!! HTML::style('packages/bower/fontawesome/css/font-awesome.min.css', [], true) !!}
     <!-- Ionicons -->
-    {!! HTML::style('packages/bower/ionicons/css/ionicons.min.css', true) !!}
+    {!! HTML::style('packages/bower/ionicons/css/ionicons.min.css', [], true) !!}
 
     @yield('styles')
 
     <!-- AdminLTE -->
-    {!! HTML::style('css/AdminLTE/AdminLTE.min.css', true) !!}
-    {!! HTML::style('css/skins/skin-black.min.css', true) !!}
+    {!! HTML::style('css/AdminLTE/AdminLTE.min.css', [], true) !!}
+    {!! HTML::style('css/skins/skin-black.min.css', [], true) !!}
 
-    {!! HTML::style('css/app.css', true) !!}
+    {!! HTML::style('css/app.css', [], true) !!}
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -59,13 +59,13 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-{!! HTML::script('packages/bower/jquery/dist/jquery.min.js', true) !!}
+{!! HTML::script('packages/bower/jquery/dist/jquery.min.js', [], true) !!}
 <!-- Bootstrap -->
-{!! HTML::script('packages/bower/bootstrap/dist/js/bootstrap.min.js', true) !!}
+{!! HTML::script('packages/bower/bootstrap/dist/js/bootstrap.min.js', [], true) !!}
 <!-- FastClick -->
-{!! HTML::script('packages/bower/fastclick/lib/fastclick.js', true) !!}
+{!! HTML::script('packages/bower/fastclick/lib/fastclick.js', [], true) !!}
 <!-- AdminLTE App -->
-{!! HTML::script('js/app.min.js', true) !!}
+{!! HTML::script('js/app.min.js', [], true) !!}
 <script>$('#flash-overlay-modal').modal();</script>
 @yield('scripts')
 </body>
