3fd0658dc7e8bcf11bf2f51c33aee78af06a21f6
Rizart Dokollari <r.dokollari@gmail.com>
2015-03-11 18:39:41 +0000

    Use heroku variable for ssl instead of default Laravel's.

--- app/Http/Middleware/Secure.php
+++ app/Http/Middleware/Secure.php
@@ -14,7 +14,8 @@ class Secure
 	 */
 	public function handle($request, Closure $next)
 	{
-		if (!$request->secure())
+		// see http://stackoverflow.com/a/25095936
+		if (!($request->header('x-forwarded-proto') <> 'https'))
 		{
 			return redirect()->secure($request->getRequestUri());
 		}
